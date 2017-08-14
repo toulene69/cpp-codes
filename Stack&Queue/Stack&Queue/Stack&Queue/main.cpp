dbce6295b8654a32f98e00f334547fa25aba7c20
Apoorv Anurag <toulene69@gmail.com>
2017-08-14 23:45:18 +0530

    Initial Commit

---
+++ Stack&Queue/Stack&Queue/Stack&Queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack&Queue
+//
+//  Created by Apoorv on 14/08/17.
+//  Copyright © 2017 presonal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
